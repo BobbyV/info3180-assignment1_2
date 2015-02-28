5b4d32cd2aea93c8380b6adaac1385d4a0262941
bobbyv <vaughnwatson22@gmail.com>
2015-02-28 04:54:45 +0000

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,9 @@ UPLOAD_FOLDER = 'app/static/img'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config.form_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xsmxlybbtsgtcg:cd8NxxCKdZMcCOSvZ8fQj0rRzk@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d7skpjjq7bn1r'
+app.config['SECRET_KEY']= "iloveyou"
 db = SQLAlchemy(app)
 
 from app import views, models
-
-app.config['SECRET_KEY'] = 'works'
